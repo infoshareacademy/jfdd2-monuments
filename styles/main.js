f40058db190b0bce0b95aa671ba30ec5d3dad2dc
szysza91 <szysza0x@gmail.com>
2016-03-12 14:16:07 +0100

    menu animowane + opisy funkcjonalnosci + literówka

--- styles/main.js
+++ styles/main.js
@@ -1,33 +1,30 @@
 $(document).ready(function () {
 
 
-$(function() {
-    //caches a jQuery object containing the header element
-    var header = $(".przycisk");
-    var logo = $(".logo");
-    var logoImg = $(".logoImg");
-        $(window).scroll(function() {
-        var scroll = $(window).scrollTop();
+//$(function() {
+//    //caches a jQuery object containing the header element
+//    var header = $(".przycisk");
+//    var logo = $(".logo");
+//    var logoImg = $(".logoImg");
+//        $(window).scroll(function() {
+//        var scroll = $(window).scrollTop();
+//
+//        if (scroll >= 100) {
+//            header.removeClass('przycisk').addClass("przyciskDol");
+//            logo.removeClass('logo').addClass("logoDol");
+//            logoImg.removeClass('logoImg').addClass("logoImgDol");
+//
+//
+//        } else {
+//            header.removeClass("przyciskDol").addClass('przycisk');
+//            logo.removeClass('logoDol').addClass("logo");
+//            logoImg.removeClass('logoImgDol').addClass("logoImg");
+//        }
+//    });
+//});
 
-        if (scroll >= 100) {
-            header.removeClass('przycisk').addClass("przyciskDol");
-            logo.removeClass('logo').addClass("logoDol");
-            logoImg.removeClass('logoImg').addClass("logoImgDol");
 
 
-        } else {
-            header.removeClass("przyciskDol").addClass('przycisk');
-            logo.removeClass('logoDol').addClass("logo");
-            logoImg.removeClass('logoImgDol').addClass("logoImg");
-        }
-    });
+
 });
-});
 
-function Person(name){
-    name = this.name;
-}
-
-Person.prototype.greet = function(otherName){
-    return "Hi " + otherName + ", my name is " + name;
-};
