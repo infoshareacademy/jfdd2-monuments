771d5fcd4fc56052754a46c008fc7549193b591d
szysza91 <szysza0x@gmail.com>
2016-03-15 17:52:16 +0100

    dodanie skryptu używanego do zmiany wielkości menu

--- styles/main.js
+++ styles/main.js
@@ -1,26 +1,26 @@
 $(document).ready(function () {
 
-  //$(function () {
-  //  //caches a jQuery object containing the header element
-  //  var header = $(".przycisk");
-  //  var logo = $(".logo");
-  //  var logoImg = $(".logoImg");
-  //  $(window).scroll(function () {
-  //    var scroll = $(window).scrollTop();
-  //
-  //    if (scroll >= 100) {
-  //      header.removeClass('przycisk').addClass("przyciskDol");
-  //      logo.removeClass('logo').addClass("logoDol");
-  //      logoImg.removeClass('logoImg').addClass("logoImgDol");
-  //
-  //
-  //    } else {
-  //      header.removeClass("przyciskDol").addClass('przycisk');
-  //      logo.removeClass('logoDol').addClass("logo");
-  //      logoImg.removeClass('logoImgDol').addClass("logoImg");
-  //    }
-  //  });
-  //});
+  $(function() {
+    //caches a jQuery object containing the header element
+    var header = $(".przycisk");
+    var logo = $(".logo");
+    var logoImg = $(".logoImg");
+    $(window).scroll(function() {
+      var scroll = $(window).scrollTop();
+
+      if (scroll >= 200) {
+        header.removeClass('przycisk').addClass("przyciskDol");
+        logo.removeClass('logo').addClass("logoDol");
+        logoImg.removeClass('logoImg').addClass("logoImgDol");
+
+
+      } else {
+        header.removeClass("przyciskDol").addClass('przycisk');
+        logo.removeClass('logoDol').addClass("logo");
+        logoImg.removeClass('logoImgDol').addClass("logoImg");
+      }
+    });
+  });
 
   function goToByScroll(id) {
 
