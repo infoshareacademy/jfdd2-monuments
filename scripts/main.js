d9b5840aaa06001cd3027cef93ae92b55d0c5eb8
Kuba Kazmierczak <loirino1234@gmail.com>
2016-03-16 18:59:04 +0100

    Podswietlane Menu/ Odpowiednia Sekcja

--- scripts/main.js
+++ scripts/main.js
@@ -20,7 +20,34 @@ $(document).ready(function () {
 
 
     });
+    $(function (){
+      var przycisk1 = $('.przycisk1');
+      var przycisk2 = $('.przycisk2');
+      var przycisk3 = $('.przycisk3');
+      var logo = $('.logo');
+      $(window).scroll(function(){
+        var scroll = $(window).scrollTop();
+
+        if (scroll >=350 && scroll <=800) {
+          przycisk1.removeClass('przyciskDol').addClass('przyciskX');
+        }else {
+          przycisk1.removeClass('przyciskX').addClass('przycisk');
+        }
+        if(scroll >= 850 && scroll <1000) {
+          przycisk2.removeClass('przyciskDol').addClass('przyciskX')
+        }else {
+          przycisk2.removeClass('przyciskX').addClass('przycisk')
+        }
+        if(scroll>1050) {
+          przycisk3.removeClass('przyciskDol').addClass('przyciskX')
+        }else {
+          przycisk3.removeClass('przyciskX').addClass('przycisk')
+        }
+      });
+
+    });
   });
+
   $(function () {
     var scrollBottom = $(window).scrollTop() + $(window).height() + 100;
     var pozycjaFunkcjonalnosci = document.getElementById('funkcjonalnosci').offsetTop;
