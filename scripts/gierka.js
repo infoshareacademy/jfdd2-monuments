8d8659455b69468d2e931dff37f76f9fa6d52781
KubaK <loirino12345@gmail.com>
2016-03-17 16:02:14 +0100

    Gierka

--- scripts/gierka.js
+++ scripts/gierka.js
@@ -1,12 +1,37 @@
-var $table = $('<table>');
+$(document).ready(function(){
+
+  var $table = $('<table>');
 $('#app').append($table);
-for (var y = 0; y < 5; y++) {
+for (var y = 0; y < 20; y++) {
   var $tr = $('<tr>');
   $table.append($tr);
-  for (var x = 0; x <5; x++) {
+  for (var x = 0; x <20; x++) {
     var $td = $('<td>');
     $td.addClass('cell');
     $tr.append($td);
   }
 }
+
+
+ function losowanie() {
+
+
+   for(var i=0;i<20;i++) {
+     var $cells = $('td:not(.zabytek)',$table);
+     var howMany = $cells.length;
+     var randomFloat = Math.random() * howMany;
+     var randomInt = Math.round(randomFloat) % howMany;
+     console.log(randomInt);
+     $cells.eq(randomInt).addClass('zabytek');
+   }
+ }
+  losowanie();
+});
+
+
+
+
+
+
+
 
