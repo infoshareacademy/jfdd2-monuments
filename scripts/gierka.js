7016c0dd9c406929ae9ea94c6d58739660cb57ca
KubaK <loirino12345@gmail.com>
2016-03-17 16:51:37 +0100

    Gierka

--- scripts/gierka.js
+++ scripts/gierka.js
@@ -18,14 +18,25 @@ for (var y = 0; y < 20; y++) {
 
    for(var i=0;i<20;i++) {
      var $cells = $('td:not(.zabytek)',$table);
-     var howMany = $cells.length;
+     var howMany = $cells.length-1;
      var randomFloat = Math.random() * howMany;
-     var randomInt = Math.round(randomFloat) % howMany;
-     console.log(randomInt);
+     var randomInt = Math.round(randomFloat) % howMany+1;
      $cells.eq(randomInt).addClass('zabytek');
    }
  }
+  $('td').eq(0).addClass('player');
   losowanie();
+
+
+  $(document).keypress(function(event){
+    console.log(event.which);
+    if (event.which == 100) {
+      $('.player').next().addClass('player').prev().removeClass('player').addClass('droga');
+    }
+    if (event.which == 97) {
+      $('.player').prev().removeClass('droga').addClass('player').next().removeClass('player').addClass('droga');
+    }
+  });
 });
 
 
