fc84e909d27a23063a7ba9e8744ff3ef1768e937
KubaK <loirino12345@gmail.com>
2016-03-18 16:23:37 +0100

    Gierka21

--- scripts/gierka.js
+++ scripts/gierka.js
@@ -29,27 +29,47 @@ for (var y = 0; y < 20; y++) {
 
 
   $(document).keypress(function(event){
-    console.log(event.which);
-    if (event.which == 100) {
-      $('.player').removeClass('player').addClass('droga').next().addClass('player');
+    var actualPosition =  $('.player');
+    var newPosition;
+    //console.log(event.which);
+    if (event.which == 100)  {
+      $('.player').removeClass('player').addClass('droga').next().addClass('player').removeClass('zabytek');
     }
     if (event.which == 97) {
-      $('.player').removeClass('player').addClass('droga').prev().removeClass('droga').addClass('player');
+      $('.player').removeClass('player').addClass('droga').prev().removeClass('droga').addClass('player').removeClass('zabytek');
     }
     if (event.which == 115) {
       var playerIndex = $('.player').index() +1 ;
-      $('.player').removeClass('player').addClass('droga').parent().next().find(':nth-child(' + playerIndex + ')').addClass('player');
+      $('.player').removeClass('player').addClass('droga').parent().next().find(':nth-child(' + playerIndex + ')').addClass('player').removeClass('zabytek');
     }
     if (event.which == 119) {
       var playerIndex = $('.player').index() +1 ;
-      $('.player').removeClass('player').addClass('droga').parent().prev().find(':nth-child(' + playerIndex + ')').addClass('player');
+      $('.player').removeClass('player').addClass('droga').parent().prev().find(':nth-child(' + playerIndex + ')').addClass('player').removeClass('zabytek');
     }
+
+
+    //Funkcja liczaca złapane zabytki
+
+
+    var points = 20-($('.zabytek')).length;
+       console.log(points);
+
+    var pokonanaDroga =  $('.droga').length;
+
+        if (pokonanaDroga > 20) {
+          alert('Koniec zdobyles' +' ' + points + ' '+ 'punkty');
+        }
+
+    //Nie powtarzająca się droga
+
+
+
 
 
     //var playerIndex = $('.player').index();
     //console.log(playerIndex)
-    var playerParentIndex = $('.player').parent().index();
-    console.log(($('.player').parent()))
+    //var playerParentIndex = $('.player').parent().index();
+    //console.log(($('.player').parent()))
 
 
 
